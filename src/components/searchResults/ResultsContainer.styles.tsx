e36cf21bfb9274e634c0df87ad32e47ff7a0a17c
victor-aunon <hola@victoraunon.com>
2023-02-02 10:30:02 +0100

    Changing ResultsContainer margin

--- src/components/searchResults/ResultsContainer.styles.tsx
+++ src/components/searchResults/ResultsContainer.styles.tsx
@@ -4,5 +4,5 @@ export const ResultsContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
   gap: 1rem;
-  margin-top: 1.5rem;
+  margin: 1rem 0;
 `
